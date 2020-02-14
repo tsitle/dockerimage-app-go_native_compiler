359b7b6ce580e00bd7f65199538e2ec25672b9ca
Thomas Sandkühler <technisandk@gmail.com>
2020-02-14 14:28:22 +0100

    Update build script to pull latest source OS Docker image

--- build_image.sh
+++ build_image.sh
@@ -168,10 +168,13 @@ if [ ! -f "cache/${LVAR_GOBIN_BN}.001" ]; then
 	done
 fi
 
+LVAR_SRC_OS_IMAGE="tsle/os-debian-${LVAR_DEBIAN_RELEASE}-${LVAR_DEBIAN_DIST}:${LVAR_DEBIAN_VERSION}"
+docker pull $LVAR_SRC_OS_IMAGE || exit 1
+echo
+
 docker build \
+		--build-arg CF_SRC_OS_IMAGE="$LVAR_SRC_OS_IMAGE" \
 		--build-arg CF_CPUARCH_DEB_DIST="$LVAR_DEBIAN_DIST" \
-		--build-arg CF_DEBIAN_RELEASE="$LVAR_DEBIAN_RELEASE" \
-		--build-arg CF_DEBIAN_VERSION="$LVAR_DEBIAN_VERSION" \
 		--build-arg CF_GOLANG_VER="$LVAR_GOLANG_VERSION" \
 		-t "$LVAR_IMAGE_NAME":"$LVAR_IMAGE_VER" \
 		.
